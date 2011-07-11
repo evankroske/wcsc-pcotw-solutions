5d23718917b065243ba4c454c816517f27da81e7
Evan Kroske <evan@evankroske.com>
2011-07-10 19:19:32 -0500

    Implemented first case

--- bridge/Main.java
+++ bridge/Main.java
@@ -1,12 +1,49 @@
 import java.io.*;
 import java.util.List;
 import java.util.LinkedList;
-import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Collections;
 
 class Main {
 	private PrintStream o;
 	private BufferedReader i;
+
+	//invariant: times is a sorted deque of crossing times
+	private int cross (Deque<Integer> times, List<String> steps) {
+		int l = times.size();
+		if (l >= 4) {
+			int a, b, c, d;
+			a = times.pollFirst();
+			b = times.pollFirst();
+			d = times.pollLast();
+			c = times.pollLast();
+			if (2 * b <= a + c) {
+				times.addFirst(b);
+				times.addFirst(a);
+
+				steps.add(String.format("%d %d", a, b));
+				steps.add(String.format("%d", a));
+				steps.add(String.format("%d %d", c, d));
+				steps.add(String.format("%d", b));
+				int time = 2 * b + a + d;
+				int remainingTime = cross(times, steps);
+				return time + remainingTime;
+			} else {
+				times.addFirst(b);
+				times.addFirst(a);
+				times.addLast(c);
+
+				steps.add(String.format("%d %d", a, d));
+				steps.add(String.format("%d", a));
+				int time = a + d;
+				int remainingTime = cross(times, steps);
+				return time + remainingTime;
+			}
+		} else {
+			o.println("Almost done");
+			return 6;
+		}
+	}
 
 	private boolean handleCase () throws IOException {
 		LinkedList<Integer> crossingTimes = new LinkedList<Integer>();
@@ -16,7 +53,13 @@ class Main {
 			crossingTimes.add(new Integer(l.trim()));
 		}
 		Collections.sort(crossingTimes);
-		System.out.println(crossingTimes);
+		List<String> steps = new LinkedList<String>();
+		int totalTime = cross(crossingTimes, steps);
+		o.println(totalTime);
+		for (String s : steps) {
+			o.println(s);
+		}
+		o.println();
 		if (l != null) {
 			return true;
 		} else {
