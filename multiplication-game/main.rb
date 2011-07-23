0862f0147c97c3b38d4f1b84dee2a36398fc0494
Evan Kroske <evan@evankroske.com>
2011-07-23 08:46:54 -0500

    Solved challenge with Ruby

--- multiplication-game/main.rb
+++ multiplication-game/main.rb
@@ -1,13 +1,29 @@
+def play (goal)
+	stan_wins, stan_win_percent = play_round(goal, 1, true)
+	winner = stan_wins ? "Stan" : "Ollie"
+	puts "#{winner} wins."
+end
+
+def play_round(goal, p, stan)
+	if p >= goal
+		return !stan, stan ? 1 : 0
+	end
+	stan_wins = {}
+	stan_win_percent = {}
+	stan_wins_if_2, stan_win_percent_if_2 = play_round(goal, p*2, !stan)
+	stan_wins_if_9, stan_win_percent_if_9 = play_round(goal, p*9, !stan)
+	if stan
+		stan_wins = if stan_win_percent_if_2 > stan_win_percent_if_9 then 
+			stan_wins_if_2 else stan_wins_if_9 end
+	else
+		stan_wins = if stan_win_percent_if_2 > stan_win_percent_if_9 then 
+			stan_wins_if_9 else stan_wins_if_2 end
+	end
+	stan_win_percent = (stan_win_percent_if_2 + stan_win_percent_if_9) / 2
+	return stan_wins, stan_win_percent
+end
+
 while l = gets
-	number = l.chomp.to_i
-	steps = 0
-	while number % 2 == 0
-		number /= 2
-		steps += 1
-	end
-	while number % 9 == 0
-		number /= 9
-		steps += 1
-	end
-	if steps % 2 == 0
-		
+	goal = l.chomp.to_i
+	play(goal)
+end
