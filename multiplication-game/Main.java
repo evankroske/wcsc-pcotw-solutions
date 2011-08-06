a400b9544402f91279a22be926952699ddb441ac
Evan Kroske <evan@evankroske.com>
2011-08-06 08:08:59 -0400

    Reorganized code.

--- multiplication-game/Main.java
+++ multiplication-game/Main.java
@@ -6,15 +6,73 @@ class Main {
 		new Main();
 	}
 
-	private class Results {
-		public boolean stanWins;
-		public double stanWinPercent;
-		public Results (boolean stanWins, double stanWinPercent) {
-			this.stanWins = stanWins;
-			this.stanWinPercent = stanWinPercent;
+
+	PrintStream out;
+	Scanner in;
+	public Main () {
+		out = System.out;
+		in = new Scanner(System.in);
+
+		while (in.hasNextLong()) {
+			play(in.nextLong());
 		}
 	}
 
+	private void play (long goal) {
+		NavigableMap<Long, Turn> turns = findWinningTurns(goal);
+
+		while (true) {
+			Map.Entry<Long, Turn> entry = turns.pollLastEntry();
+			Turn turn = entry.getValue();
+			if (turn.isOne()) {
+				out.printf("%s wins.\n", turn.winner());
+				break;
+			}
+			for (int n : Arrays.asList(2, 9)) {
+				if (turn.p % n != 0) {
+					continue;
+				}
+				long p = turn.p / n;
+				Turn parent = turns.get(p);
+				if (parent == null) {
+					parent = new Turn(p);
+					turns.put(p, parent);
+				}
+				parent.addBranch(n, turn);
+			}
+		}
+	}
+
+	private NavigableMap<Long, Turn> findWinningTurns(long goal) {
+		NavigableMap<Long, Turn> turns = new TreeMap<Long, Turn>();
+		long p = 1, pSteps = 0;
+		while (true) {
+			long q = p;
+			long qSteps = pSteps;
+			while (true) {
+				q *= 9;
+				qSteps++;
+				if (q >= goal) {
+					boolean stan = qSteps % 2 != 0;
+					boolean stanWins = stan;
+					double stanWinPercent = stanWins ? 1 : 0;
+					turns.put(q, new Turn(stan, stanWins, stanWinPercent, q));
+					break;
+				}
+			}
+			p *= 2;
+			pSteps++;
+			if (p >= goal) {
+				boolean stan = pSteps % 2 != 0;
+				boolean stanWins = stan;
+				double stanWinPercent = stanWins ? 1 : 0;
+				turns.put(p, new Turn(stan, stanWins, stanWinPercent, p));
+				break;
+			}
+		}
+		return turns;
+	}
+	
 	private class Turn implements Comparable<Turn> {
 		private boolean stan;
 		private boolean stanWins;
@@ -86,70 +144,4 @@ class Main {
 	}
 			
 
-
-	PrintStream out;
-	Scanner in;
-	public Main () {
-		out = System.out;
-		in = new Scanner(System.in);
-
-		while (in.hasNextLong()) {
-			play(in.nextLong());
-		}
-	}
-
-	private void play (long goal) {
-		NavigableMap<Long, Turn> turns = findWinningTurns(goal);
-
-		while (true) {
-			Map.Entry<Long, Turn> entry = turns.pollLastEntry();
-			Turn turn = entry.getValue();
-			if (turn.isOne()) {
-				out.printf("%s wins.\n", turn.winner());
-				break;
-			}
-			for (int n : Arrays.asList(2, 9)) {
-				if (turn.p % n != 0) {
-					continue;
-				}
-				long p = turn.p / n;
-				Turn parent = turns.get(p);
-				if (parent == null) {
-					parent = new Turn(p);
-					turns.put(p, parent);
-				}
-				parent.addBranch(n, turn);
-			}
-		}
-	}
-
-	private NavigableMap<Long, Turn> findWinningTurns(long goal) {
-		NavigableMap<Long, Turn> turns = new TreeMap<Long, Turn>();
-		long p = 1, pSteps = 0;
-		while (true) {
-			long q = p;
-			long qSteps = pSteps;
-			while (true) {
-				q *= 9;
-				qSteps++;
-				if (q >= goal) {
-					boolean stan = qSteps % 2 != 0;
-					boolean stanWins = stan;
-					double stanWinPercent = stanWins ? 1 : 0;
-					turns.put(q, new Turn(stan, stanWins, stanWinPercent, q));
-					break;
-				}
-			}
-			p *= 2;
-			pSteps++;
-			if (p >= goal) {
-				boolean stan = pSteps % 2 != 0;
-				boolean stanWins = stan;
-				double stanWinPercent = stanWins ? 1 : 0;
-				turns.put(p, new Turn(stan, stanWins, stanWinPercent, p));
-				break;
-			}
-		}
-		return turns;
-	}
 }
