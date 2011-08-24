7ae07854373bcd0d46ae1597f412f316a9fbbb6c
Evan Kroske <evan@evankroske.com>
2011-08-24 08:21:28 -0400

    Another solution fails

---
+++ how-many-pieces/main.rb
@@ -0,0 +1,27 @@
+def factorial (n)
+	return 1 if n < 1
+	(1..n).inject {|o, p| o * p}
+end
+
+def choose (n, k)
+	return 0 if n < k
+	factorial(n) / (factorial(k) * factorial(n - k))
+end
+
+def count_regions (num_points)
+	puts "num_points = #{num_points}"
+	num_lines = num_points * (num_points - 1) / 2
+	puts "num_lines = #{num_lines}"
+	intersections_per_point = choose(num_points - 1, 2) - 1
+	pairs_on_edge = choose(num_points, 2)
+	intersections_on_edge = num_points * intersections_per_point
+	intersections = choose(num_lines, 2) - intersections_on_edge - pairs_on_edge
+	puts "num_intersections = #{intersections}"
+	puts num_lines + intersections + 1
+end
+
+STDIN.gets
+while l = STDIN.gets
+	num_points = l.chomp.to_i
+	count_regions(num_points)
+end
