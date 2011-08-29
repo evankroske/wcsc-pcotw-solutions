8d78294e6a09628acce24287ef51027ec7ae3738
Evan Kroske <evan@evankroske.com>
2011-08-28 22:40:52 -0400

    Looked up formula for number of intersections. Facepalm.

--- how-many-pieces/main.rb
+++ how-many-pieces/main.rb
@@ -12,9 +12,7 @@ def count_regions (num_points)
 	puts "num_points = #{num_points}"
 	num_lines = num_points * (num_points - 1) / 2
 	puts "num_lines = #{num_lines}"
-	intersections_per_point = choose(num_points - 3, 2)
-	intersections_on_edge = num_points * intersections_per_point
-	intersections = choose(num_lines - num_points, 2) - intersections_on_edge
+	intersections = choose(num_points, 4)
 	puts "num_intersections = #{intersections}"
 	puts num_lines + intersections + 1
 end
