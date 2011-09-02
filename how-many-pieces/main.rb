186ac4564674de172b8b1173def069149bd2dcae
Evan Kroske <evan@evankroske.com>
2011-09-02 00:01:00 -0400

    Modified "choose" function to accept inputs up to 2^31.

--- how-many-pieces/main.rb
+++ how-many-pieces/main.rb
@@ -1,11 +1,21 @@
-def factorial (n)
-	return 1 if n < 1
-	(1..n).inject {|o, p| o * p}
-end
-
 def choose (n, k)
 	return 0 if n < k
-	factorial(n) / (factorial(k) * factorial(n - k))
+	return $p_triangle[n][k]
+end
+
+$p_triangle
+def generate_binomial_coefficients (max_n)
+	$p_triangle = Array.new(max_n+1)
+	(0...max_n+1).each do |row_i|
+		$p_triangle[row_i] = Array.new(row_i+1)
+		$p_triangle[row_i][0] = 1
+		$p_triangle[row_i][row_i] = 1
+	end
+	(2...max_n+1).each do |n|
+		(1...n).each do |k|
+			$p_triangle[n][k] = $p_triangle[n-1][k-1] + $p_triangle[n-1][k]
+		end
+	end
 end
 
 def count_regions (num_points)
@@ -17,8 +27,10 @@ def count_regions (num_points)
 	puts num_lines + intersections + 1
 end
 
-STDIN.gets
-while l = STDIN.gets
-	num_points = l.chomp.to_i
-	count_regions(num_points)
+def main
+	STDIN.gets
+	while l = STDIN.gets
+		num_points = l.chomp.to_i
+		count_regions(num_points)
+	end
 end
