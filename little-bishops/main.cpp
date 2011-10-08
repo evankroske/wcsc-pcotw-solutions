1c2c6b7fbd8b95b4f52a09418754ea595bfab7cb
Evan Kroske <evan@evankroske.com>
2011-10-08 00:00:56 -0400

    Implemented algorithm and caused segmentation fault.

--- little-bishops/main.cpp
+++ little-bishops/main.cpp
@@ -1,41 +1,89 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
+int numSolutions = 0;
+
+int countBishops (vector<int> & a)
+{
+	int numBishops = 0;
+	vector<int>::iterator i;
+	for (i = a.begin(); i != a.end(); i++) {
+		numBishops += (*i != INT_MAX) ? 1 : 0;
+	}
+	return numBishops;
+}
+
 bool isSolution (vector<int> a)
 {
-	return true;
+	return countBishops(a) == 3;
 }
 
 void processSolution (vector<int> a)
 {
+	numSolutions++;
 }
 
-vector<int> genCandidates (vector<int>a, int n, int k)
+int countSquares (int n, int k)
 {
-	return vector<int>();
+	if (k <= n / 2 - 1 + n % 2) {
+		return k + 1;
+	} else {
+		return n - k;
+	}
 }
 
-void genSolutions (vector<int> a, int n, int k)
+void genCandidates (vector<int> a, int n, int maxBishops, int k, vector<int> & candidates)
+{
+	int numBishops = countBishops(a);
+	int numSquares = countSquares(n, k);
+	if (numBishops < maxBishops) {
+		for (int i = 0; i < numSquares; i++) {
+			int candidateIndex = i - numSquares / 2;
+			for (int j = (numSquares - 1) % 2; j < k; j += 2) {
+				int bishopIndex = a.at(j);
+				if (bishopIndex == INT_MAX) {
+					candidates.push_back(candidateIndex);
+				} else if (bishopIndex != candidateIndex) {
+					candidates.push_back(candidateIndex);
+				}
+			}
+		}
+	}
+	candidates.push_back(INT_MAX);
+}
+
+void genSolutions (vector<int> & a, int n, int maxBishops, int k)
 {
 	if (isSolution(a)) {
 		processSolution(a);
 		return;
 	}
-	vector<int> candidates = genCandidates(a, n, k);
+	vector<int> candidates;
+	genCandidates(a, n, maxBishops, k, candidates);
 	vector<int>::iterator i;
 	for (i = candidates.begin(); i != candidates.end(); i++) {
 		int c = *i;
 		a.push_back(c);
-		genSolutions(a, n, k);
+		genSolutions(a, n, maxBishops, k+1);
 		a.pop_back();
 	}
 }
 
 int main () 
 {
-	cout << "Hello, world!\n";
+	while (true) {
+		int n, numBishops;
+		cin >> n >> numBishops;
+		if (n == 0) {
+			break;
+		}
+		vector<int> solutions;
+		genSolutions(solutions, n, numBishops, 0);
+		cout << numSolutions << "\n";
+		numSolutions = 0;
+	}
 	return 0;
 }
-
